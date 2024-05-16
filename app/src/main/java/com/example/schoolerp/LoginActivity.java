c8443f110df3572b74a7dd3065d92edab8969af8
Dharani <dharani.h26@gmail.com>
2024-05-16 15:50:02 +0530

    Updated

--- app/src/main/java/com/example/schoolerp/LoginActivity.java
+++ app/src/main/java/com/example/schoolerp/LoginActivity.java
@@ -87,7 +87,7 @@ public class LoginActivity extends AppCompatActivity {
                 signup.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
-                                Intent intent = new Intent(LoginActivity.this, Home_erp.class);
+                                Intent intent = new Intent(LoginActivity.this, SendOTPActivity.class);
                                 startActivity(intent);
                         }
                 });
